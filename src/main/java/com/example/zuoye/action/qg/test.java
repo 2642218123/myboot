e2205f7f93b6991c0825b48fe824d4df567b8d6d
yangqi <17097445676@163.com>
2019-01-04 10:56:32 +0800

    第一次提交boot项目

---
+++ src/main/java/com/example/zuoye/action/qg/test.java
@@ -0,0 +1,11 @@
+package com.example.zuoye.action.qg;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("1");
+		System.out.println("2");
+	}
+
+}
